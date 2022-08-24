105a6018972875495277968a60c21057229f7836
originalspring <105029639+originalspring@users.noreply.github.com>
2022-08-24 02:32:09 -0700

    new

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+/* For main.js */
+
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./sw.js');
+    }
+  }
